66bf708f1d8b489b0395755bb65688b3d4d75379
BHANUMAKUMAR <142152820+BHANUMAKUMAR@users.noreply.github.com>
2025-08-26 20:49:01 +0530

    Create Code1_Zigzag_Conversion_LeetCode_6.py

---
+++ Day-7/Code1_Zigzag_Conversion_LeetCode_6.py
@@ -0,0 +1,18 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows==1 or numRows>=len(s):
+            return s
+        l=[""]*numRows
+        index=0
+        step=1
+        for i in s:
+            l[index]+=i
+            if index==0:
+                step=1
+            elif index==numRows-1:
+                step=-1
+            index +=step
+        return "".join(l)
+
+
+        
